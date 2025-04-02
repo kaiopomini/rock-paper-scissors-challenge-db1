b1a7177626a3d886ce7b2c7ef17c65f98fb34ec1
Wellington Albino <wellington.albino@db1.com.br>
2025-04-02 15:59:12 -0300

    [MVAR-656] fix: adding logic for game mode against the computer

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ let score2 = 0;
 let currentPlayer = 1;
 let player1Choice = "";
 let player2Choice = "";
+let gameModeSelected = ""
 let playHistory = JSON.parse(localStorage.getItem("playHistory") || "[]");
 
 const result = document.getElementById("result");
@@ -11,9 +12,26 @@ const score2Element = document.getElementById("score2");
 const continueButton = document.getElementById("continue");
 const restartButton = document.getElementById("restart");
 const historyButton = document.getElementById("history");
+const gameMode = document.getElementById("game-mode")
+const options = document.getElementById("options")
+const pvp = document.getElementById("pvp")
+const pve = document.getElementById("pve")
+const allButtons = document.querySelectorAll("button");
 
 const player1Choices = document.querySelectorAll("#player1 .choice");
 const player2Choices = document.querySelectorAll("#player2 .choice");
+
+function disableAllButtons(condition) {
+  allButtons.forEach((button) => {
+    if (button !== pvp && button !== pve) {
+      button.disabled = condition;
+    }
+  });
+}
+
+function enableGameButtons() {
+  disableAllButtons(false)
+}
 
 function playRound() {
   if (player1Choice === player2Choice) {
@@ -85,7 +103,7 @@ function disabledChoices(conditionPlayer1, conditionPlayer2) {
 }
 
 player1Choices.forEach((choice) => {
-  disabledChoices(false, true)
+  disabledChoices(false, true);
   choice.addEventListener("click", () => {
     if (currentPlayer !== 1 || player1Choice) return;
 
@@ -93,17 +111,26 @@ player1Choices.forEach((choice) => {
     alert("Jogador 1 escolheu: " + player1Choice);
     currentPlayer = 2;
     disabledChoices(true, false);
+
+    if (gameModeSelected === "pve") {
+      setTimeout(() => {
+        player2Choice = ["pedra", "papel", "tesoura"][Math.floor(Math.random() * 3)];
+        alert("Computador escolheu: " + player2Choice);
+        playRound();
+        disabledChoices(true, true);
+      }, 1000);
+    }
   });
 });
 
 player2Choices.forEach((choice) => {
   choice.addEventListener("click", () => {
-    if (currentPlayer !== 2 || player2Choice) return;
+    if (currentPlayer !== 2 || player2Choice || gameModeSelected !== "pvp") return;
 
     player2Choice = choice.dataset.choice;
     alert("Jogador 2 escolheu: " + player2Choice);
     playRound();
-    disabledChoices(true, true)
+    disabledChoices(true, true);
   });
 });
 
@@ -121,7 +148,10 @@ restartButton.addEventListener("click", () => {
   disabledChoices(false, true)
   clearPlayHistory()
   updateScore();
+  disableAllButtons(true)
   result.textContent = "";
+  gameMode.style.display = 'flex'
+  options.style.display = 'none'
 });
 
 historyButton.addEventListener("click", () => {
@@ -135,4 +165,21 @@ historyButton.addEventListener("click", () => {
   return alert("Histórico vazio!");
 });
 
+pvp.addEventListener("click", () => {
+  gameMode.style.display = "none";
+  options.style.display = "flex";
+  gameModeSelected = "pvp";
+  enableGameButtons();
+  disabledChoices(false, true);
+});
+
+pve.addEventListener("click", () => {
+  gameMode.style.display = "none";
+  options.style.display = "flex";
+  gameModeSelected = 'pve'
+  enableGameButtons();
+  disabledChoices(false, true);
+})
+
 updateScore();
+disableAllButtons(true);
